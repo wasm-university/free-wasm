37d77913ea5651f33131d7392a0245382a017107
Adrian Cole <adrian@tetrate.io>
2022-08-30 13:33:27 +0800

    deps: updates wazero to 1.0.0-pre.4
    This updates [wazero](https://wazero.io/) to [1.0.0-pre.4](https://github.com/tetratelabs/wazero/releases/tag/v1.0.0-pre.4).
    
    Notably, v1.0.0-pre.4:
    * improves module initialization speed
    * supports listeners in the compiler engine
    * supports WASI `fd_pread`, `fd_readdir` and `path_filestat_get`
    * breaks GoModuleFunc API
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- wasi-nodejs/02-string-from-wasm/hello.go
+++ wasi-nodejs/02-string-from-wasm/hello.go
@@ -2,15 +2,14 @@ package main
 
 // Declare a main function, this is the entrypoint into our go module
 // That will be run.
-func main() { }
+func main() {}
 
 //export hello
 func hello() *byte {
-  return &(([]byte)("hello world")[0])
+	return &(([]byte)("hello world")[0])
 }
 
 //export hey
 func hey() *byte {
-  return &(([]byte)("hey people")[0])
+	return &(([]byte)("hey people")[0])
 }
-
